eed22d29325f222bb81dea6f22cab2b4b3bceb03
TanmoyTheBoT <88877195+TanmoyTheBoT@users.noreply.github.com>
2026-09-13 04:30:54 +0600

    refactor: Extract version info to header file for easier updates

---
+++ resources/version_rc.h
@@ -0,0 +1,17 @@
+#ifndef APP_VERSION_RC_H
+#define APP_VERSION_RC_H
+
+#define VER_FILEVERSION             0,0,1,0
+#define VER_FILEVERSION_STR         "0.0.1.0"
+
+#define VER_PRODUCTVERSION          0,0,1,0
+#define VER_PRODUCTVERSION_STR      "0.0.1"
+
+#define VER_COMPANYNAME_STR         "TanmoyTheBoT"
+#define VER_FILEDESCRIPTION_STR     "TOTP Manager - Secure 2FA Authentication"
+#define VER_INTERNALNAME_STR        "TOTPManager"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (C) 2026 TanmoyTheBoT"
+#define VER_ORIGINALFILENAME_STR    "TOTPManager.exe"
+#define VER_PRODUCTNAME_STR         "TOTP Manager"
+
+#endif // APP_VERSION_RC_H
